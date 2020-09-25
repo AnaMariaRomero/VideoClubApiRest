022e70da6602b48a2a549412b641a47fe8b512e5
Romers Ani <anneromeroflcl@gmail.com>
2020-09-24 23:40:01 -0300

    Modificaciones en nombres de métodos

--- TestVideoClub/TestVideoClub1.cs
+++ TestVideoClub/TestVideoClub1.cs
@@ -2,11 +2,9 @@
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using VideoClubApiRest.Api.Controllers;
 using VideoClubApiRest.Core.Entities;
-using VideoClubApiRest.Core.Interfaces;
 using VideoClubApiRest.Infraestructure.Data;
 using VideoClubApiRest.Infraestructure.Repositories;
 using Xunit;
@@ -29,7 +27,7 @@ namespace TestVideoClub
 
         }
         [Fact]
-        public async Task It_should_add_a_rent_successfully_into_dataBaseAsync()
+        public async Task ItShouldAddARentSuccefullyIntoDatabaseAsync()
         {
             var dbContext = CreateDatabase();
             //Arrange
@@ -37,7 +35,6 @@ namespace TestVideoClub
             var rent = new Rents { RentId = 1, ClientId = (new Guid()).ToString(), ObjectId = (new Guid()).ToString(), Detailssatus = "RENT", Detailsuntil = "2010/01/05" };
             //Act
             await sut.InsertRents(rent);
-
             //Assert
             Assert.NotNull(sut);
             //Clean up
@@ -45,7 +42,7 @@ namespace TestVideoClub
         }
 
         [Fact]
-        public void It_should_get_a_rent_successfully_into_dataBaseAsync()
+        public void ItShouldGetArentSuccessfullyIntoDataBaseAsync()
         {
             var dbContext = CreateDatabase();
             //Arrange
@@ -56,20 +53,17 @@ namespace TestVideoClub
             var rent1 = new Rents { RentId = 3, ClientId = (new Guid()).ToString(), ObjectId = (new Guid()).ToString(), Detailssatus = "RENT", Detailsuntil = "2010/01/05" };
             //Act
             sut.InsertRents(rent1);
-            
             var rentResult = sut.GetRents();
             Assert.NotNull(rentResult);
-
             //Assert
             Assert.NotNull(sut);
             Assert.IsAssignableFrom<Task<IEnumerable<Rents>>>(rentResult);
-
             //Clean up
             dbContext.Dispose();
         }
 
         [Fact]
-        public async Task It_should_add_a_rent_With_RentsController_Result_Ok()
+        public async Task ItShouldAddARentWithRentsControllerResultOk()
         {
             var dbContext = CreateDatabase();
            
@@ -89,17 +83,13 @@ namespace TestVideoClub
             ObjectResult result1 = result as ObjectResult;
             //Assert
             await Assert.IsAssignableFrom<Task<IActionResult>>(result2);
-            Assert.NotEqual(201, result1.StatusCode);
-            
-            
-            
-            
+            Assert.Equal(200, result1.StatusCode);
             //Clean up
             dbContext.Dispose();
         }
 
         [Fact]
-        public async Task It_should_add_a_rent_With_Data_Exist_RentsController_Result_Fails()
+        public async Task ItShouldAddARentWithDataExistRentsControllerResultFails()
         {
             var dbContext = CreateDatabase();
 
@@ -111,12 +101,9 @@ namespace TestVideoClub
             await controller.InsertRents(rent);
             var rent1 = new Rents { RentId = 2, ClientId = (new Guid()).ToString(), ObjectId = (new Guid()).ToString(), Detailssatus = "RENT", Detailsuntil = "2010/01/05" };
             //Act
-            //controller.InsertRents(rent1);
             var rent2 = new Rents { RentId = 1, ClientId = (new Guid()).ToString(), ObjectId = (new Guid()).ToString(), Detailssatus = "RENT", Detailsuntil = "2010/01/05" };
             //Act
-           
             await controller.InsertRents(rent1);
-
             await Assert.ThrowsAsync< InvalidOperationException>(() => controller.InsertRents(rent2));
             //Clean up
             dbContext.Dispose();
